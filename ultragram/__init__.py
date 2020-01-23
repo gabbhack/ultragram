b6cea0d45e172a9bcc4093ad048698228306809c
Gabben <43146729+gabbhack@users.noreply.github.com>
2020-01-23 21:36:50 +0500

    Update __init__.py

--- ultragram/__init__.py
+++ ultragram/__init__.py
@@ -1,5 +1,5 @@
 """
-Ultragram - is ultra fast Telegram Bot API library for development both large, blazing fast and scalable bots.
+Ultragram - is modern, ultra fast (ultra-high-performance), easy to learn, fast to code and funny Telegram Bot API framework for development both large, blazing fast and scalable bots.
 """
 
 __version__ = "0.0.1"
